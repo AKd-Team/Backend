478f847518c162ef10bcc5782b36185a1e40eee7
bomtelecom <mvirgilnicolae@yahoo.com>
2020-12-02 19:03:08 +0200

    Register works! Login works

--- Academic/Entities/academicContext.cs
+++ Academic/Entities/academicContext.cs
@@ -38,7 +38,7 @@ namespace Academic.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql(Configuration.GetConnectionString("Db"));
+                optionsBuilder.UseNpgsql("Host=localhost;Database=academic;Username=postgres;Password=BiggieSmalls");
             }
         }
 
