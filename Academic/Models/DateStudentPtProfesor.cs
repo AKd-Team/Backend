2009af269c870846ba41a1ce3adc82ba9f535746
AnaMunteanu <50970055+AnaMunteanu@users.noreply.github.com>
2020-12-11 20:10:00 +0200

    Model date student si un mic update pe returnarea listei de materii

---
+++ Academic/Models/DateStudentPtProfesor.cs
@@ -0,0 +1,25 @@
+﻿using Academic.Entities;
+
+namespace Academic.Models
+{
+    public class DateStudentPtProfesor
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Grupa { get; set; }
+        public string Semigrupa { get; set; }
+        public string Specializare { get; set; }
+        public string Facultate { get; set; }
+
+        public DateStudentPtProfesor(Student student, string grupa, string semigrupa, string specializare, string facultate)
+        {
+            Nume = student.Nume;
+            Prenume = student.Prenume;
+            Grupa = grupa;
+            Semigrupa = semigrupa;
+            Specializare = specializare;
+            Facultate = facultate;
+
+        }
+    }
+}
--- Academic/Services/ProfesorService.cs
+++ Academic/Services/ProfesorService.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Academic.Entities;
 using Academic.Helpers;
+using Academic.Models;
 using Microsoft.Extensions.Options;
 
 namespace Academic.Services
@@ -10,7 +12,7 @@ namespace Academic.Services
     public interface IProfesorService
     {
         Users GetById(int id);
-        public IEnumerable<Orarmaterie> GetListMaterii(int IdProfesor);
+        public IEnumerable<Materie> GetListMaterii(int IdProfesor);
     }
     public class ProfesorService : IProfesorService
     {
@@ -27,10 +29,36 @@ namespace Academic.Services
             return _context.Users.Find(id);
         }
 
-        public IEnumerable<Orarmaterie> GetListMaterii(int IdProfesor)
+        public IEnumerable<Materie> GetListMaterii(int IdProfesor)
         {
-            //var listMaterii = _context.Orarmaterie.SingleOrDefault(o=>o.IdProfesor==IdProfesor);
-            return _context.Orarmaterie.Where(o => (o.IdProfesor == IdProfesor)).ToList();
+            var orarmaterii = _context.Orarmaterie.Where(o => (o.IdProfesor == IdProfesor)).ToList();
+            var list_materii = new HashSet<Materie>();
+            foreach (var o in orarmaterii)
+            {
+                var materie = _context.Materie.Find(o.IdMaterie);
+                list_materii.Add(materie);
+            }
+
+            return list_materii;
         }
+
+        /*public IEnumerable<DateStudentPtProfesor> GetStudentiInscrisi(int idMaterie, int idProfesor)
+        {
+            var orarmaterie = _context.Orarmaterie.Where(o => (o.IdMaterie == idMaterie && o.IdProfesor == idProfesor)).ToList();
+            var listIdFormatieSpecializare = new HashSet<Tuple<Int32, Int32>>();
+            //var list_studenti = new List<Student>();
+            foreach (var ora in orarmaterie)
+            {
+                var IdFormatieSpecializare = new Tuple<Int32, Int32>(ora.IdFormatie, ora.IdSpecializare);
+                listIdFormatieSpecializare.Add(IdFormatieSpecializare);
+            }
+
+            foreach (var IdFormatieSpecializare in listIdFormatieSpecializare)
+            {
+                var list_studenti = _context.Student.SingleOrDefault(s =>
+                    (s.IdFormatie == IdFormatieSpecializare.Item1 && s.IdSpecializare == IdFormatieSpecializare.Item2));
+
+            }
+        }*/
     }
 }
